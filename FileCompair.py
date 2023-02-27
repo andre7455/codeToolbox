925360626ecb1c747058eeeb45870f1cd6e59c33
andre7455 <andrevanderlugt@hotmail.com>
2023-02-27 02:49:58 +0100

    Update FileCompair.py

--- FileCompair.py
+++ FileCompair.py
@@ -10,20 +10,30 @@ def compare_files(file1, file2, hashes):
 
 def delete_duplicate_files():
     hashes = {}
+    files_processed = set()
     for root, dirs, files in os.walk('.'):
         for file in files:
             full_path = os.path.join(root, file)
             with open(full_path, 'rb') as f:
                 hashes[full_path] = hashlib.md5(f.read()).hexdigest()
+            files_processed.add(full_path)
 
     for root, dirs, files in os.walk('.'):
         for file1 in files:
             full_path1 = os.path.join(root, file1)
-            for file2 in files:
-                full_path2 = os.path.join(root, file2)
-                if full_path1 != full_path2 and compare_files(full_path1, full_path2, hashes):
-                    os.remove(full_path2)
-                    print(f"Deleted {full_path2}")
+            if full_path1 in files_processed:
+                file_to_delete = None
+                for file2 in files_processed - set([full_path1]):
+                    if compare_files(full_path1, file2, hashes):
+                        if file_to_delete is None:
+                            file_to_delete = file2
+                        else:
+                            if os.stat(file2).st_mtime < os.stat(file_to_delete).st_mtime:
+                                file_to_delete = file2
+                if file_to_delete is not None:
+                    os.remove(file_to_delete)
+                    print(f"Deleted {file_to_delete}")
+                    files_processed.remove(file_to_delete)
 
 while True:
     try:
