a69c62746988e354fadfe28628cb26d1ce0f28e7
andre7455 <andrevanderlugt@hotmail.com>
2023-02-27 02:49:19 +0100

    Update FileCompair.py

--- FileCompair.py
+++ FileCompair.py
@@ -9,21 +9,21 @@ def compare_files(file1, file2, hashes):
     return False
 
 def delete_duplicate_files():
-    files = os.listdir()
     hashes = {}
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            full_path = os.path.join(root, file)
+            with open(full_path, 'rb') as f:
+                hashes[full_path] = hashlib.md5(f.read()).hexdigest()
 
-    for file in files:
-        with open(file, 'rb') as f:
-            hashes[file] = hashlib.md5(f.read()).hexdigest()
-
-    while files:
-        file1 = files.pop(0)
-        for file2 in files[:]:
-            if compare_files(file1, file2, hashes):
-                os.remove(file2)
-                print(f"Deleted {file2}")
-                files = os.listdir()
-                break
+    for root, dirs, files in os.walk('.'):
+        for file1 in files:
+            full_path1 = os.path.join(root, file1)
+            for file2 in files:
+                full_path2 = os.path.join(root, file2)
+                if full_path1 != full_path2 and compare_files(full_path1, full_path2, hashes):
+                    os.remove(full_path2)
+                    print(f"Deleted {full_path2}")
 
 while True:
     try:
