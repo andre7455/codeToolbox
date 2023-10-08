5d02ee442d27c405344f67ea1454ec9d3db24588
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2023-10-08 12:00:25 +0200

    Create dockerMagnetDownloader.sh
    made a script that starts a docker container that downloads a magnet link and than cleans up after itself.
    
    it drops the downloaded files into the dir where the script was run

---
+++ dockerMagnetDownloader.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Check if a magnet link is provided as an argument
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <magnet_link>"
+  exit 1
+fi
+
+MAGNET_LINK="$1"
+
+# Create a Docker container to download the torrent via the magnet link
+CONTAINER_NAME="transmission-container"
+DOWNLOAD_DIR=$PWD
+
+docker run --rm -d \
+  --name "$CONTAINER_NAME" \
+  -v "$DOWNLOAD_DIR":/downloads \
+  -e PUID=$(id -u) \
+  -e PGID=$(id -g) \
+  -e TZ=Europe/Amsterdam \
+  -e TRANSMISSION_WEB_HOME=/etc/transmission/web \
+  -e USER= \
+  -e PASS= \
+  -e WHITELIST= \
+  -e PEERPORT= \
+  -e HOST_WHITELIST= \
+  linuxserver/transmission
+
+# Wait for 10 seconds to allow the container to start
+sleep 20
+
+# Add the magnet link to Transmission
+docker exec "$CONTAINER_NAME" transmission-remote -a "$MAGNET_LINK"
+
+# Start the loop in the background
+(
+while true; do
+    STATUS=$(docker exec "$CONTAINER_NAME" transmission-remote -l | awk 'NR>1 {print $2}')
+    # Check if $2 contains "100%"
+    if [[ "$STATUS" == *100%* ]]; then
+      echo "Download completed. Stopping the container..."
+      sleep 60
+      docker stop "$CONTAINER_NAME"
+      docker rm "$CONTAINER_NAME"
+      docker image rm linuxserver/transmission
+      break
+    fi
+    sleep 10
+done
+
+# Move and remove folders after the loop finishes
+mv "$DOWNLOAD_DIR"/complete/* "$DOWNLOAD_DIR"/
+rmdir complete/
+rmdir incomplete/
+) &
