0110f12ca18355c64bde41165be9d47739111dbf
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2023-10-23 00:00:56 +0200

    Update JPGtoPNG.py

--- JPGtoPNG.py
+++ JPGtoPNG.py
@@ -1,6 +1,8 @@
 import os
 
-for filename in os.listdir():
-    if filename.endswith('.jpg'):
-        new_filename = filename[:-4] + '.png'
-        os.system(f'sips -s format png {filename} --out {new_filename}')
+current_directory = os.getcwd()  # Get the current working directory
+
+for filename in os.listdir(current_directory):
+    if filename.lower().endswith(('.jpg', '.jpeg')):
+        new_filename = os.path.splitext(filename)[0] + '.png'
+        os.system(f'sips -s format png "{os.path.join(current_directory, filename)}" --$
